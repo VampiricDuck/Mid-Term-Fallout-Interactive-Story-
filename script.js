a0faf1d1ee00f7610d9fe00e7cfbab773ea942d8
Caden Rogers _ Student - WakeIBTechEC <crogers@students.wcpss.net>
2026-03-10 14:08:47 -0400

    my work for the day

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+// Character Data //
+let sex ;
+let str = 5;
+let per = 5;
+let end = 5;
+let cha = 5;
+let int = 5;
+let agi = 5;
+let luc = 5;
+let strmod = str - 5;
+let permod = per - 5;
+let endmod = end - 5;
+let chamod = cha - 5;
+let intmod = int - 5;
+let agimod = agi - 5;
+let lucmod = luc - 5;
+let level = 1;
+let scorept = 3;
+let xp = 0;
+let hp = end + 10;
+let sp = agi + 10;
+let ap = agi + 10;
+
+// character creation //
